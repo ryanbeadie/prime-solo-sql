2a8a6ac39962ed58bcb8ebb46dd684cb2975cac4
Ryan Beadie <ryansteelebeadie@yahoo.com>
2017-04-26 11:30:45 -0500

    done

--- solution.sql
+++ solution.sql
@@ -9,9 +9,12 @@ UPDATE syntax_practice SET account_balance='10.00'WHERE account_balance='0.00' A
 -- 4. Select all users that have attempted 9 or more transactions.
 SELECT * FROM syntax_practice WHERE transactions_attempted >'8';
 -- 5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+SELECT username, account_balance FROM syntax_practice ORDER BY account_balance DESC LIMIT 3
 -- 6. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT username, account_balance FROM syntax_practice ORDER BY account_balance DESC LIMIT 3
 -- 7. Get all users with account balances that are more than $100.
 SELECT * FROM syntax_practice WHERE account_balance>100;
 -- 8. Add a new record.
 INSERT INTO syntax_practice (username, city,  transactions_completed, transactions_attempted, account_balance ) VALUES ('Joe Walsh', 'Los Angeles', '100', '75', '1000000');
 -- 9. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+DELETE from syntax_practice where city='phoenix' OR city='miami' and transactions_completed <5
